e60dfcca0551f3993d7a1db178d233ca9a7ebe5b
Shohei Aoki <shoaok@gmail.com>
2015-08-21 15:56:12 +0900

    Change class name

--- lib/sinatra/strap.rb
+++ lib/sinatra/strap.rb
@@ -1,24 +1,26 @@
 require "sinatra/strap/version"
 
 module Sinatra
-  class Strap
+  module Strap
+    class Base
 
-    def generate(appname)
-      if  File.directory?(appname)
-        system("mkdir #{appname}")
-        system("cp -r #{File.dirname(__FILE__)}/../vendor/public #{appname}")
-        system("cp -r #{File.dirname(__FILE__)}/../vendor/views #{appname}")
-        system("cp #{File.dirname(__FILE__)}/../vendor/app.rb #{appname}")
-      else
-        puts "app folder already exists."
+      def generate(appname)
+        if  File.directory?(appname)
+          system("mkdir #{appname}")
+          system("cp -r #{File.dirname(__FILE__)}/../vendor/public #{appname}")
+          system("cp -r #{File.dirname(__FILE__)}/../vendor/views #{appname}")
+          system("cp #{File.dirname(__FILE__)}/../vendor/app.rb #{appname}")
+        else
+          puts "app folder already exists."
+        end
       end
-    end
 
-    def show_help 
-      puts "Usage:"
-      puts "    sinatra-strap new APP_PATH"
-      exit(0)
-    end
+      def show_help 
+        puts "Usage:"
+        puts "    sinatra-strap new APP_PATH"
+        exit(0)
+      end
 
+    end
   end
 end
